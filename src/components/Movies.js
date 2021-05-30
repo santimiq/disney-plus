fa23cad4872de7c71599fae8670cd97963604800
Santiago Miqueo <smiqueo@hotmail.com>
2021-05-30 17:29:46 -0500

    Movies component

---
+++ src/components/Movies.js
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+
+function Movies() {
+  return (
+    <Container>
+      <h4>Recommendend for You</h4>
+      <Content>
+        <Wrap>
+          <img
+            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/97EA0DC528229223E07704C0B6CF88CC508CAD531063B307D3F5365618A487AD/scale?width=400&aspectRatio=1.78&format=jpeg"
+            alt=""
+          />
+        </Wrap>
+        <Wrap>
+          <img
+            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/97EA0DC528229223E07704C0B6CF88CC508CAD531063B307D3F5365618A487AD/scale?width=400&aspectRatio=1.78&format=jpeg"
+            alt=""
+          />
+        </Wrap>
+        <Wrap>
+          <img
+            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/97EA0DC528229223E07704C0B6CF88CC508CAD531063B307D3F5365618A487AD/scale?width=400&aspectRatio=1.78&format=jpeg"
+            alt=""
+          />
+        </Wrap>
+        <Wrap>
+          <img
+            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/97EA0DC528229223E07704C0B6CF88CC508CAD531063B307D3F5365618A487AD/scale?width=400&aspectRatio=1.78&format=jpeg"
+            alt=""
+          />
+        </Wrap>
+      </Content>
+    </Container>
+  );
+}
+
+export default Movies;
+
+const Container = styled.div`
+  overflow: hidden;
+  padding: 30px 0px 26px;
+`;
+const Content = styled.div`
+  display: grid;
+  grid-gap: 25px;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+`;
+const Wrap = styled.div`
+  border-radius: 10px;
+  cursor: pointer;
+  overflow: hidden;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(1.05);
+  }
+`;
